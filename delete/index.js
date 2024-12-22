9a637ecd4c3eae72fa0b1fdda71b3dfd84b5250f
Sam Chutter <samchutter@MacBook-Pro-7.local>
2024-12-22 14:49:41 -0600

    Initial commit

---
+++ delete/index.js
@@ -0,0 +1,12 @@
+function morgansSexy (in1, in2){
+    if(in1===in2){
+        console.log("Hello Morgan");
+    }else if()
+        else)
+        
+}
+
+function morgansSexy (in1, in2){
+    var result;
+    return `Hello ${result ? 'Goodbye' : 'Hello'};`
+}
